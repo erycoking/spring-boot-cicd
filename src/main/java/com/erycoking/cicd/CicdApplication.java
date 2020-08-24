481724851fb539183441b32615f41e16fe12c8b6
erycoking <erycokin360@gmail.com>
2020-08-24 10:20:23 +0300

    Initial commit

---
+++ src/main/java/com/erycoking/cicd/CicdApplication.java
@@ -0,0 +1,23 @@
+package com.erycoking.cicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@SpringBootApplication
+@RestController
+public class CicdApplication {
+
+	@GetMapping("/hello")
+	public Mono<ResponseEntity<String>> sayHello() {
+		return Mono.just(ResponseEntity.ok().body("Hi there!!!"));
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdApplication.class, args);
+	}
+
+}
